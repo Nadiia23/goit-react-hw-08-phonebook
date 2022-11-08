a109dcac41da89ac31b48e66605995140e9b8cd6
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-08 15:41:04 +0200

    Create contactsOperations.js

---
+++ src/redux/contactsOperations.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { contactsAPI } from "helpers/API";
+
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll",
+    async (_, thunkAPI) => {
+        try {
+            const { data } = await contactsAPI.get('/contacts/contacts');
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.massage)
+        }
+    }
+);
+
+export const addContact = createAsyncThunk(
+    "contacts/addContact",
+    async ({ name, phone}, thunkAPI) => {
+        try {
+            const { data } = await contactsAPI.post('/contacts/contacts', { name, phone});
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.massage)
+        }
+    }
+);
+
+export const deleteContact = createAsyncThunk(
+    "contacts/deleteContact",
+    async (id, thunkAPI) => {
+        try {
+            const { data } = await contactsAPI.delete(`/contacts/contacts/${id}`);
+            return data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.massage)
+        }
+    }
+);
