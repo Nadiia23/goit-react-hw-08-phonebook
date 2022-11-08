9d9f1044a38323e5877acda7a2704b312cfad3b1
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-08 15:40:15 +0200

    ContactForm

--- src/components/Form/ContactForm.js
+++ src/components/Form/ContactForm.js
@@ -1,60 +1,56 @@
-import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from "react-redux";
-import { addContact } from "redux/contactsSlice";
-import { getContacts } from 'redux/selector';
-// import { getContacts } from "redux/selectors";
+import { addContact } from "redux/contactsOperations";
 import s from './contactForm.module.css';
+import { selectContacts } from "redux/selector";
 
-const Phonebook = () => {
-   const dispatch = useDispatch();
-    const contacts = useSelector(getContacts);
+export default function ContactForm() {
+
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
+    
     const handleSubmit = event => {
         event.preventDefault();       
 
         const form = event.target;
+        const name = form.elements.name.value;
+        const phone = form.elements.phone.value
         
         const existingContact = contacts.find(contact =>
-            contact.name === form.elements.name.value
+            contact.name === name
         );
 
         if (existingContact) {      
-            alert(`${form.elements.name.value} is already in contacts`)
+            alert(`${name} is already in contacts`)
             form.reset();
             return 
         }
 
-        dispatch(addContact(form.elements.name.value, form.elements.number.value));
+        dispatch(addContact({name, phone}));
 
         form.reset();
-    }
+    }    
 
     return (
         <form className={s.form} onSubmit={handleSubmit}>
-          <label className={s.label} htmlFor="name">Name</label>
-          <input className={s.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-
-          <label className={s.label} htmlFor="input">Number</label>
-          <input className={s.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-
-          <button className={s.btnAdd} type="submit">Add contact</button>
+            <label className={s.label} htmlFor="name">Name</label>
+            <input
+                className={s.input}
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+            />
+            <label className={s.label}  htmlFor="number">Number</label>
+            <input
+                className={s.input}
+                type="tel"
+                name="phone"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+            />
+            <button className={s.btnAdd} type="submit">Add contact</button>
         </form>
-    );
-  }
-
-Phonebook.propTypes = {
-  onSubmit: PropTypes.func,
-};
-
-export default Phonebook;
+    )
+}
