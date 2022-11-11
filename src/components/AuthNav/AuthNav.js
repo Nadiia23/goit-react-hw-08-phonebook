9f3feea359b2e5400da1193781f85418b1ab5fe7
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-11 13:46:10 +0200

    Create AuthNav.js

---
+++ src/components/AuthNav/AuthNav.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+export const AuthNav = () => {
+    return (
+        <div>
+            <NavLink>
+                Register
+            </NavLink>
+            <NavLink>
+                Log in
+            </NavLink>
+        </div>
+    )
+}
