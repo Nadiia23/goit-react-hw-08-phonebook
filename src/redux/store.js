379fcde0ee27b51d008a9f80345d7660404aadd7
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-11-11 13:47:49 +0200

    Update redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "./contactsSlice";
-import { filterReducer } from "./filterSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import { contactsReducer } from './Contacts/contactsSlice';
+import { filterReducer } from './Contacts/filterSlice';
+import storage from 'redux-persist/lib/storage';
+// import persistReducer from "redux-persist/es/persistReducer";
+import { authReducer } from './Auth/slice';
+// import persistStore from "redux-persist/es/persistStore";
+
+// const middleware = [
+//   ...getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// ];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
     filter: filterReducer,
-  }
-})
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
